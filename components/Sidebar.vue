bda26691af0bab631782b4db38fc3fe9f3c8d911
Limmie <limmiecrazzy@gmail.com>
2021-05-21 23:38:04 +0500

    Project initialization: styles, fonts, and configs

---
+++ components/Sidebar.vue
@@ -0,0 +1,5 @@
+<template>
+  <aside id="sidebar">
+    <h1>This is sidebar</h1>
+  </aside>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+    <div id="main-wrapper">
+      <Sidebar />
+      <Nuxt />
+    </div>
+  </div>
+</template>
