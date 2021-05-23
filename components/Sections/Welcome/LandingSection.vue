7c9065daa783d2a45cd115f5da36de0804e9ab4c
Limmie <limmiecrazzy@gmail.com>
2021-05-24 00:02:57 +0500

    Landing section with styles

---
+++ components/Sections/Welcome/LandingSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <section id="landing-section" class="welcome-section">
+    <div id="landing-title" v-html="$t('sections.landing.title')"></div>
+    <div
+      id="landing-description"
+      v-html="$t('sections.landing.description')"
+    ></div>
+    <button class="default-button" id="landing-button">
+      <span>
+        {{ $t("viewPortfolio") }}
+        <fa-icon :icon="['fas', 'briefcase']" />
+      </span>
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "LandingSection",
+};
+</script>
