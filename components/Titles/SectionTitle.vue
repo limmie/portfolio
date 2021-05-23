abc261b4375fb59d53b4750f520d5e0e1198ba27
Limmie <limmiecrazzy@gmail.com>
2021-05-24 00:05:38 +0500

    Section title component created, remove unuseful files and main.sass imports

---
+++ components/Titles/SectionTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="welcome-section-title">
+    <p>{{ navName }}</p>
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectionTitle",
+
+  props: {
+    navName: {
+      type: String,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
