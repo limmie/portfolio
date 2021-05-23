26bfacb67f40286979472788bf5bbeffdf512609
Limmie <limmiecrazzy@gmail.com>
2021-05-24 00:03:46 +0500

    Skills section with styles

---
+++ components/Sections/Welcome/SkillsSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <section id="skills-section" class="welcome-section">
+    <SectionTitle
+      :navName="$t('skills')"
+      :title="$t('sections.skills.title')"
+    />
+  </section>
+</template>
+
+<script>
+import SectionTitle from "../../Titles/SectionTitle";
+
+export default {
+  components: {
+    SectionTitle,
+  },
+};
+</script>
