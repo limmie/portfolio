8daf08c71bd95a06a15b738f1d4951ae7004e5a1
Limmie <limmiecrazzy@gmail.com>
2021-05-28 14:38:02 +0500

    Code optimization, added pink color

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,6 +45,8 @@ export default {
             "faHandshake",
             "faLightbulb",
             "faComments",
+            "faShareAlt",
+            "faEye",
           ],
         },
       },
