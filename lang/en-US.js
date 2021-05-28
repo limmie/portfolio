ff23d98129e41f12188d5e836c555acb07a71ab0
Limmie <limmiecrazzy@gmail.com>
2021-05-28 14:39:22 +0500

    main.sass imports, main pages section component imports, language updates

--- lang/en-US.js
+++ lang/en-US.js
@@ -17,7 +17,7 @@ export default {
 
   sections: {
     landing: {
-      title: "<h1>Hello</h1><h1>I'm</h1><h1>Limmie</h1>",
+      title: "<h1>Hello</h1><h1>I'm</h1><h1>Selim</h1>",
       description:
         "<p>100% effective websites written with </p> <p> Vue(Nuxt) and node.js(Express)</p>",
     },
@@ -25,7 +25,7 @@ export default {
     about: {
       title: "Who am i?",
       text:
-        "<p><strong> Hi I'm Limmie Blaine </strong> On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word  and the Little Blind Text should turn around and return to its own, safe country. </p> <br> <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar </p>",
+        "<p class='welcome-section-text'><strong> Hi I'm Limmie Blaine </strong> On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word  and the Little Blind Text should turn around and return to its own, safe country. </p> <br> <p class='welcome-section-text'>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar </p>",
       bannerText:
         "<h2>I am happy to know you</h2><h2> that 300+ projects done successfully!</h2>",
     },
@@ -88,6 +88,76 @@ export default {
 
     skills: {
       title: "My speciality",
+      text:
+        "The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.",
+    },
+
+    experience: {
+      title: "Work Experience",
+      items: [
+        {
+          name: "Full Stack Web Developer",
+          timeline: "2019-2020",
+          text:
+            "Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.",
+          color: "blue",
+          icon: ["fas", "server"],
+        },
+        {
+          name: "Full Stack Web Developer",
+          timeline: "2019-2020",
+          text:
+            "Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.",
+          color: "red",
+          icon: ["fas", "server"],
+        },
+        {
+          name: "Full Stack Web Developer",
+          timeline: "2019-2020",
+          text:
+            "Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.",
+          color: "purple",
+          icon: ["fas", "server"],
+        },
+      ],
+    },
+
+    works: {
+      title: "My recent works",
+
+      nav: [
+        { name: "Frontend" },
+        { name: "Backend" },
+        { name: "Full-stack" },
+        { name: "Server" },
+      ],
+
+      items: [
+        {
+          name: "Website 1",
+          types: ["Frontend", "Backend"],
+          image: "/images/sections/welcome/works/work-1.webp",
+          views: 100,
+        },
+        {
+          name: "Website 1",
+          types: ["Frontend", "Backend"],
+          image: "/images/sections/welcome/works/work-1.webp",
+          views: 100,
+        },
+        {
+          name: "Website 1",
+          types: ["Frontend", "Backend"],
+          image: "/images/sections/welcome/works/work-1.webp",
+          views: 100,
+        },
+        {
+          name: "Website 1",
+          types: ["Frontend", "Backend"],
+          image: "/images/sections/welcome/works/work-1.webp",
+          views: 100,
+        },
+      ],
     },
   },
 
--- pages/welcome/index.vue
+++ pages/welcome/index.vue
@@ -4,6 +4,8 @@
     <AboutSection />
     <ServicesSection />
     <SkillsSection />
+    <ExperienceSection />
+    <WorksSection />
   </main>
 </template>
 
@@ -12,6 +14,8 @@ import LandingSection from "../../components/Sections/Welcome/LandingSection";
 import AboutSection from "../../components/Sections/Welcome/AboutSection";
 import ServicesSection from "../../components/Sections/Welcome/ServicesSection";
 import SkillsSection from "../../components/Sections/Welcome/SkillsSection";
+import ExperienceSection from "../../components/Sections/Welcome/ExperienceSection";
+import WorksSection from "../../components/Sections/Welcome/WorksSection";
 
 export default {
   components: {
@@ -19,6 +23,8 @@ export default {
     AboutSection,
     ServicesSection,
     SkillsSection,
+    ExperienceSection,
+    WorksSection,
   },
 };
 </script>
