c41800f54714ab7786aae0f030f094f38a833fc3
Limmie <limmiecrazzy@gmail.com>
2021-05-31 13:56:20 +0500

    Hamburger component has created and used in mobile sidebar

---
+++ components/Hamburger.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    data-do-not-close-sidebar="true"
+    class="hamburger hamburger--spin"
+    :class="active ? 'is-active' : null"
+    type="button"
+    @click="$emit('clicked')"
+  >
+    <span class="hamburger-box" data-do-not-close-sidebar="true">
+      <span class="hamburger-inner" data-do-not-close-sidebar="true"></span>
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    active: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -3,11 +3,11 @@
     <div id="sidebar-wrapper">
       <div id="sidebar-freelancer">
         <div id="sidebar-freelancer-avatar">
-          <img src="/images/avatars/sidebar/sidebar-avatar.jpg" alt="" />
+          <img src="/images/avatars/sidebar/sidebar-avatar.webp" alt="" />
         </div>
         <div id="sidebar-freelancer-info">
-          <h1>Limmie Blaine</h1>
-          <p><span>Full Stack Web Developer</span> based in Turkmenistan</p>
+          <h1>{{ this.$t("fullName") }}</h1>
+          <p v-html="$t('sidebar.freelancerInfo')"></p>
         </div>
       </div>
 
@@ -15,6 +15,17 @@
         <a v-for="(item, index) in nav" :key="index" :href="item.link">
           {{ item.name }}
         </a>
+
+        <nuxt-link
+          id="sidebar-nav-language-selector"
+          :to="switchLocalePath(nextLanguageCode)"
+        >
+          {{ $t("currentLanguage") }}
+          <img
+            :src="`/images/countries/${$t('currentLanguageIconName')}`"
+            alt=""
+          />
+        </nuxt-link>
       </nav>
       <footer id="sidebar-footer">
         <p v-html="this.$t('footer.rightsReserved')"></p>
@@ -29,16 +40,25 @@ export default {
   computed: {
     nav() {
       return [
-        { name: this.$t("home"), link: "#" },
-        { name: this.$t("about"), link: "#" },
-        { name: this.$t("services"), link: "#" },
-        { name: this.$t("skills"), link: "#" },
-        { name: this.$t("education"), link: "#" },
-        { name: this.$t("experience"), link: "#" },
-        { name: this.$t("work"), link: "#" },
-        { name: this.$t("blog"), link: "#" },
-        { name: this.$t("contact"), link: "#" },
+        { name: this.$t("home"), link: "#landing-section" },
+        { name: this.$t("about"), link: "#about-section" },
+        { name: this.$t("services"), link: "#services-section" },
+        { name: this.$t("skills"), link: "#skills-section" },
+        { name: this.$t("experience"), link: "#experience-section" },
+        { name: this.$t("works"), link: "#works-section" },
+        { name: this.$t("contacts"), link: "#contacts-section" },
       ];
+    },
+
+    languages() {
+      return [
+        { name: "Русский", code: "ru" },
+        { name: "English", code: "en" },
+      ];
+    },
+
+    nextLanguageCode() {
+      return this.$i18n.locale === "en" ? "ru" : "en";
     },
   },
 };
