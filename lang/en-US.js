3ef1a6ee75ee473cf0bf35f3bb9d8415bd1c6ca0
Limmie <limmiecrazzy@gmail.com>
2021-05-31 13:55:25 +0500

    Language update, removed turkmen version

--- lang/en-US.js
+++ lang/en-US.js
@@ -1,13 +1,21 @@
 export default {
+  currentLanguage: "English",
+  currentLanguageIconName: "usa.png",
+
+  fullName: "Selim Ataballyev",
+
   home: "Home",
   about: "About",
   services: "Services",
   skills: "Skills",
-  education: "Education",
   experience: "Experience",
-  work: "Work",
-  blog: "Blog",
-  contact: "Contact",
+  works: "Works",
+  contacts: "Contacts",
+
+  sidebar: {
+    freelancerInfo:
+      "<span>Full Stack Web Developer</span> based in Turkmenistan",
+  },
 
   footer: {
     rightsReserved: `© Copyright |  All rights reserved |  ${new Date().getFullYear()}`,
@@ -17,7 +25,7 @@ export default {
 
   sections: {
     landing: {
-      title: "<h1>Hello</h1><h1>I'm</h1><h1>Selim</h1>",
+      title: "<h1>Hello, </h1><h1>I'm</h1><h1>Selim</h1>",
       description:
         "<p>100% effective websites written with </p> <p> Vue(Nuxt) and node.js(Express)</p>",
     },
@@ -25,9 +33,17 @@ export default {
     about: {
       title: "Who am i?",
       text:
-        "<p class='welcome-section-text'><strong> Hi I'm Limmie Blaine </strong> On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word  and the Little Blind Text should turn around and return to its own, safe country. </p> <br> <p class='welcome-section-text'>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar </p>",
+        "<p class='welcome-section-text'><strong> Hi, I'm Selim Ataballyev</strong> I'm talented full-stack web developer with over 2+ years of experiences in wide range of websites. </p> " +
+        "<br> " +
+        "<p class='welcome-section-text'>I don’t like to define myself by the work I’ve done. I define myself by the work I want to do. Skills can be taught, personality is inherent. I prefer to keep learning, continue challenging myself, and do interesting things that matter</p>" +
+        "<br>" +
+        "<p class='welcome-section-text'>Fueled by high energy levels and boundless enthusiasm, I’m easily inspired and more then willing to follow my fascinationswherever they take me. I’m passionate, expressive, multi-talentedspirit with a natural ability to entertain and inspire. I’m never satisfied to just come up with ideas. Instead I have an almostimpulsive need to act on them</p>" +
+        "<br>" +
+        "<p class='welcome-section-text'>My abundant energy fuels me in the pursuit of many interests, hobbies, areas of study and artistic endeavors. I’m a fast learner, able to pick up new skills and juggle different projects and roles with relative ease. I like to develop expertise in a number of areas over the course of my life and career.</p>" +
+        "<br>" +
+        "<p class='welcome-section-text'>You can also call me a <strong>frontend developer, backend developer, system administrator,</strong> or by any other market defined function-title.</p>",
       bannerText:
-        "<h2>I am happy to know you</h2><h2> that 300+ projects done successfully!</h2>",
+        "<h2>I am happy to know you</h2><h2> that 15+ projects done successfully!</h2>",
     },
 
     services: {
@@ -37,87 +53,81 @@ export default {
         {
           name: "Planning",
           description:
-            "Separated they live in Bookmarksgrove right at the coast of the Semantics",
+            "Project planning from scratch and further improvements, creation of ideas",
           icon: ["fas", "lightbulb"],
           color: "blue",
         },
         {
           name: "Consulting",
           description:
-            "Separated they live in Bookmarksgrove right at the coast of the Semantics",
+            "Discussion of a project: what technologies are most suitable, what terms, prices, etc.",
           icon: ["fas", "comments"],
           color: "red",
         },
         {
           name: "Frontend",
           description:
-            "Separated they live in Bookmarksgrove right at the coast of the Semantics",
+            "Site layout from Photoshop, Figma, etc. with clean HTML, CSS + Javascript, Vue.js (Nuxt)",
           icon: ["fas", "code"],
           color: "yellow",
         },
         {
           name: "Backend",
           description:
-            "Separated they live in Bookmarksgrove right at the coast of the Semantics",
+            "Backend of website with Node.js (Express). For projects that are in a hurry I can offer server-side website built with EJS",
           icon: ["fas", "layer-group"],
           color: "purple",
         },
         {
           name: "Database",
           description:
-            "Separated they live in Bookmarksgrove right at the coast of the Semantics",
+            "Selection of a database and its structure, and its further development (MySQL, PostgreSQL, MongoDB",
           icon: ["fas", "database"],
           color: "aloe",
         },
         {
           name: "Server",
           description:
-            "Separated they live in Bookmarksgrove right at the coast of the Semantics",
+            "Configuration of both client-side or server-side website with Nginx in VPS (virtual private server)",
           icon: ["fas", "server"],
           color: "darkblue",
         },
       ],
 
       stats: [
-        { quantity: 309, name: "Cups of coffee" },
-        { quantity: 356, name: "Projects" },
+        { quantity: 125, name: "Cups of coffee" },
+        { quantity: 16, name: "Projects" },
         { quantity: 30, name: "Clients" },
-        { quantity: 10, name: "Partners" },
+        { quantity: 3, name: "Partners" },
       ],
     },
 
     skills: {
       title: "My speciality",
       text:
-        "The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.",
+        "<p>As you can see, I use <strong>innovative technologies</strong> to translate your ideas into reality </p> " +
+        "<br> " +
+        "<p>I try to <strong>write my codes as clean as possible</strong>, it is the most valuable skill of programmer. In frontend, <strong>I do not use layout libraries like Bootstrap or Vuetify</strong>, because they often pollute my code and interferes with free design</p>",
     },
 
     experience: {
       title: "Work Experience",
       items: [
         {
-          name: "Full Stack Web Developer",
-          timeline: "2019-2020",
+          name: "Ideal Hyzmat",
+          timeline: "2020-2021",
           text:
-            "Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.",
+            "My first job as junior web developer. My first mistakes and successes were right here. I got a lot of experience working with a team of gifted programmers. Website of <a href='http://idealhyzmat.com/' target='_blank'> Ideal Hyzmat</a> ",
           color: "blue",
-          icon: ["fas", "server"],
+          icon: ["fas", "suitcase"],
         },
         {
-          name: "Full Stack Web Developer",
-          timeline: "2019-2020",
+          name: "Geek Space",
+          timeline: "2021-now",
           text:
-            "Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.",
+            "Firstly I was participating as volunteer web developer, but then I got a job. Now, I'm working with small, friendly team. Website of <a href='https://geekspace.dev' target='_blank'> Geek space</a>",
           color: "red",
-          icon: ["fas", "server"],
-        },
-        {
-          name: "Full Stack Web Developer",
-          timeline: "2019-2020",
-          text:
-            "Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.",
-          color: "purple",
-          icon: ["fas", "server"],
+          icon: ["fas", "lightbulb"],
         },
       ],
     },
@@ -159,10 +169,19 @@ export default {
         },
       ],
     },
+    contact: {},
   },
 
   viewPortfolio: "View portfolio",
   hireMe: "Hire me",
+  loadMore: "Load more",
+  getInTouch: "Get in touch",
+  sendMessage: "Send message",
+
+  name: "Name",
+  email: "Email",
+  subject: "Subject",
+  message: "Message",
 
   frontend: "Frontend",
   backend: "Backend",
--- lang/ru-RU.js
+++ lang/ru-RU.js
@@ -1,11 +1,189 @@
 export default {
+  currentLanguage: "Русский",
+  currentLanguageIconName: "russia.png",
+
+  fullName: "Селим Атабаллыев",
+
   home: "Главная",
   about: "Обо мне",
   services: "Услуги",
-  skills: "Skills",
-  education: "Education",
-  experience: "Experience",
-  work: "Work",
-  blog: "Blog",
-  contact: "Contact",
+  skills: "Навыки",
+  experience: "Опыт",
+  works: "Работы",
+  contacts: "Контакты",
+
+  sidebar: {
+    freelancerInfo: "<span>Full Stack Web Разработчик</span> из Туркменистана",
+  },
+
+  footer: {
+    rightsReserved: `© Авторские права | Все права защищены |  ${new Date().getFullYear()}`,
+    designIsMadeBy:
+      "Дизайн сделан <a href='https://colorlib.com' target='_blank'>Colorlib</a>",
+  },
+
+  sections: {
+    landing: {
+      title: "<h1>Привет, </h1><h1>ЯСелим</h1>",
+      description:
+        "<p> 100% эффективные веб-сайтов, написанные на </p> <p> Vue (Nuxt) и node.js (Express) </p>",
+    },
+
+    about: {
+      title: "Кто я?",
+      text:
+        "<p class='welcome-section-text'> <strong> Привет, меня зовут Селим Атабаллев. </strong> Я талантливый full-stack веб-разработчик с 2-летним опытом работы с широким спектром веб-сайтов. </p> " +
+        "<br> " +
+        "<p class='welcome-section-text'> Я не хочу определять себя по проделанной работе. Я определяю себя работой, которой хочу заниматься. Навыкам можно научить, личность присуща. Я предпочитаю продолжать учиться, продолжать ставить себя перед собой и заниматься интересными делами, которые имеют значение </p>" +
+        "<br>" +
+        "<p class='welcome-section-text'> Подпитываемый высоким уровнем энергии и безграничным энтузиазмом, я легко вдохновляюсь и более чем готов следовать своим увлечениям, куда бы они меня ни привели. Я  выразительный, разносторонне одаренный человек с естественной способностью вдохновлять. Мне никогда не нравится просто придумывать идеи. Вместо этого у меня есть почти импульсивная потребность воплощать их </p>" +
+        "<br>" +
+        "<p class='welcome-section-text'> Моя обильная энергия подпитывает меня в стремлении ко многим интересам, хобби, областям учебы и творчеству. Я быстро учусь, могу осваивать новые навыки и относительно легко совмещать разные проекты и роли. Мне нравится накапливать опыт во многих областях в течение моей жизни и карьеры. </p>" +
+        "<br>" +
+        "<p class='welcome-section-text'> Вы также можете называть меня <strong> фронтенд-разработчиком, серверным разработчиком, системным администратором </strong> или по любому другому определенному на рынке названию программистов. </p>",
+      bannerText:
+        "<h2> Я рад сообщить, </h2> <h2>что более 15 проектов успешно выполнены! </h2>",
+    },
+
+    services: {
+      title: "Вот некоторые из моих услуг",
+
+      items: [
+        {
+          name: "Planning",
+          description:
+            "Project planning from scratch and further improvements, creation of ideas",
+          icon: ["fas", "lightbulb"],
+          color: "blue",
+        },
+        {
+          name: "Консультации",
+          description:
+            "Обсуждение проекта: какие технологии подходят больше всего, на какие сроки, цены и т.д.",
+          icon: ["fas", "comments"],
+          color: "red",
+        },
+        {
+          name: "Фронтэнд (интерфейс)",
+          description:
+            "Верстка сайта из Photoshop, Figma и т.д. с чистым HTML, CSS + Javascript, Vue.js (Nuxt)",
+          icon: ["fas", "code"],
+          color: "yellow",
+        },
+        {
+          name: "Бэкэнд",
+          description:
+            "Бэкэнд сайта с Node.js (Express). Для проектов, которые нужно сделать, могу предложить серверный сайт, созданный на EJS.",
+          icon: ["fas", "layer-group"],
+          color: "purple",
+        },
+        {
+          name: "База данных",
+          description:
+            "Выбор базы данных и ее структуры, и ее дальнейшее развитие (MySQL, PostgreSQL, MongoDB",
+          icon: ["fas", "database"],
+          color: "aloe",
+        },
+        {
+          name: "Сервер",
+          description:
+            "Настройка как клиентского, так и серверного веб-сайта с Nginx в VPS (виртуальный частный сервер)",
+          icon: ["fas", "server"],
+          color: "darkblue",
+        },
+      ],
+
+      stats: [
+        { quantity: 125, name: "Чашек кофе" },
+        { quantity: 16, name: "Проектов" },
+        { quantity: 30, name: "Клиентов" },
+        { quantity: 3, name: "Партнеров" },
+      ],
+    },
+
+    skills: {
+      title: "Моя специальность",
+      text:
+        "<p> Как видите, я использую <strong> инновационные технологии </strong>, чтобы воплотить ваши идеи в жизнь </p> " +
+        "<br> " +
+        "<p> Я стараюсь <strong> писать свои коды как можно более чисто </strong>, это самый ценный навык программиста. Во внешнем интерфейсе <strong> я не использую библиотеки макетов, такие как Bootstrap или Vuetify </strong>, потому что они часто загрязняют мой код и мешают свободному дизайну </p>",
+    },
+
+    experience: {
+      title: "Рабочий стаж",
+      items: [
+        {
+          name: "Ideal Hyzmat",
+          timeline: "2020-2021",
+          text:
+            "Моя первая работа младшим веб-разработчиком. Именно здесь были мои первые ошибки и успехи. Я получил большой опыт после работы с командой талантливых программистов. Веб-сайт <a href='http://idealhyzmat.com/' target='_blank'> Ideal Hyzmat </a>",
+          color: "blue",
+          icon: ["fas", "suitcase"],
+        },
+        {
+          name: "Geek Space",
+          timeline: "2021-now",
+          text:
+            "Сначала я был веб-разработчиком-волонтером, но потом устроился на работу. Сейчас работаю с небольшой дружной командой. Веб-сайт <a href='https://geekspace.dev' target='_blank'> Geek space </a>",
+          color: "red",
+          icon: ["fas", "lightbulb"],
+        },
+      ],
+    },
+
+    works: {
+      title: "Мои последние работы",
+
+      nav: [
+        { name: "Фронтэнд" },
+        { name: "Бэкэнд" },
+        { name: "Full-stack" },
+        { name: "Сервер" },
+      ],
+
+      items: [
+        {
+          name: "Website 1",
+          types: ["Frontend", "Backend"],
+          image: "/images/sections/welcome/works/work-1.webp",
+          views: 100,
+        },
+        {
+          name: "Website 1",
+          types: ["Frontend", "Backend"],
+          image: "/images/sections/welcome/works/work-1.webp",
+          views: 100,
+        },
+        {
+          name: "Website 1",
+          types: ["Frontend", "Backend"],
+          image: "/images/sections/welcome/works/work-1.webp",
+          views: 100,
+        },
+        {
+          name: "Website 1",
+          types: ["Frontend", "Backend"],
+          image: "/images/sections/welcome/works/work-1.webp",
+          views: 100,
+        },
+      ],
+    },
+    contact: {},
+  },
+
+  viewPortfolio: "Показать портфолио",
+  hireMe: "Нанять меня",
+  loadMore: "Загрузить больше",
+  getInTouch: "Связаться",
+  sendMessage: "Отправить",
+
+  name: "Имя",
+  email: "Электронная почта",
+  subject: "Тема",
+  message: "Сообщение",
+
+  frontend: "Фронтэнд",
+  backend: "Бэкэнд",
+  database: "База данных",
+  server: "Сервер",
 };
